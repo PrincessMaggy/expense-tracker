41a55dd059da8e48c72cf45559c329518e0d2b1b
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-25 08:25:58 +0100

    created tracker component

---
+++ src/components/Tracker/Tracker.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Tracker extends Component{
+
+    render(){
+        return(
+            <>Tracker</>
+        )
+    }
+}
+
+export default Tracker;
