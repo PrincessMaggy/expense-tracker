3b77ef975bc03ca068b429e9ac8ae71e0fe7e3b8
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-25 08:54:56 +0100

    linked login to firebase

--- src/components/Forms/Login.js
+++ src/components/Forms/Login.js
@@ -1,28 +1,49 @@
 import React, { Component } from "react";
+import fire from "../../config/Fire";
 
 class Login extends Component {
     state = {
         email: "",
         password: "",
-        fileErrors: ""
+        fireErrors: ""
+    }
+
+    handleChange =(e) =>{
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    login =(e)=>{
+        e.preventDefault();
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch((error)=>{
+            this.setState({fireErrors: error.message})
+        })
     }
 
     render(){
+
+        let errorNotify = this.state.fireErrors?
+        (<div className="errors">{this.state.fireErrors}</div>): null
         return (
             <>
+            {errorNotify}
             <form>
                 <input type="text" 
                 className="reg" 
                 placeholder="Email" 
+                value={this.state.email}
+                onChange={this.handleChange}
                 name="email"/>
 
                 <input type="password" 
                 className="reg" 
-                placeholder="Password" 
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.handleChange} 
                 name="password"/>
 
                 <input type="submit"
                  className="submitBtn" 
+                 onClick={this.login}
                  value="ENTER"/>
             </form>
             </>
