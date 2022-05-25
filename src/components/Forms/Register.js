dd69d860b89da7f8bb6702a148a45f1a5e6565b4
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-25 08:16:41 +0100

    linked form data to firebase

--- src/components/Forms/Register.js
+++ src/components/Forms/Register.js
@@ -1,30 +1,61 @@
 import React, { Component } from "react";
+import fire from "../../config/Fire";
 
 class Register extends Component {
     state = {
         email: "",
         password: "",
         displayName:"",
-        fileErrors: ""
+        fireErrors: ""
     }
 
+    handleChange =(e) =>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    register =(e) =>{
+        e.preventDefault();
+        //firebase hooks
+        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((user) =>{
+            let currentUser = fire.auth().currentUser;
+            currentUser.updateProfile({
+                displayName: this.state.displayName
+            })
+        }).catch((error) =>{
+            this.setState({fireErrors:error.message})
+        });
+    }
+
+
     render(){
+
+        let errorNotify = this.state.fireErrors ? 
+        (<div className="errors"> {this.state.fireErrors}</div>) :null;
         return (
             <>
+            {errorNotify}
             <form>
                 <input type="text" 
                 className="reg" 
                 placeholder="Your Name" 
-                name="name"/>
+                onChange={this.handleChange}
+                value={this.state.displayName}
+                name="displayName"/>
 
                 <input type="text" 
                 className="reg" 
                 placeholder="Email" 
+                onChange={this.handleChange}
+                value={this.state.email}
                 name="email"/>
 
                 <input type="password" 
                 className="reg" 
-                placeholder="Password" 
+                placeholder="Password"
+                onChange={this.handleChange}
+                value={this.state.password} 
                 name="password"/>
 
                 <input type="text" 
@@ -50,6 +81,7 @@ class Register extends Component {
 
                 <input type="submit"
                  className="submitBtn" 
+                 onClick={this.register}
                  value="REGISTER"/>
             </form>
             </>
