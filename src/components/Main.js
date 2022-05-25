5ad99b12d4157e81f44318c35f9408f3d60a533d
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-25 08:37:59 +0100

    effect tracker component loading

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ import '../styles/main.scss';
 import fire from "../config/Fire";
 import Login from './Forms/Login';
 import Register from "./Forms/Register";
+import Tracker from "./Tracker/Tracker";
 
 export default class Main extends Component{
 
@@ -36,11 +37,14 @@ export default class Main extends Component{
             formSwitch: action === 'register'? true: false
         })
     } 
+
+
     render(){
         const form = !this.state.formSwitch ? <Login/> : <Register/>;
         return(
         <>
-                <div className="main">
+            {!this.state.user?
+                (<div className="main">
                     {form}
                     {!this.state.formSwitch ? ( <span className="extra">
                         Not Registered?  <button 
@@ -53,7 +57,7 @@ export default class Main extends Component{
                     </span>)
 
                     }
-                </div>
-        </>)
+                </div>) : (<Tracker/>)
+      }  </>)
     }
 }
