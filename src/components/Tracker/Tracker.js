dce5db7a1738c29aa20abfb942d96d53d35ad32b
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-26 07:51:00 +0100

    added structure for main page

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -10,7 +10,55 @@ class Tracker extends Component{
     render(){
         return(
             <>
-            <button onClick={this.logout}>LOG OUT</button>
+            
+
+            <div className="trackerBlock">
+                <div className="welcome">
+                    <span>Hi, username</span>
+                    <button onClick={this.logout} className="exit">LOG OUT</button>
+                </div>
+
+                <div className="totalCash">$100</div>
+
+
+                <div className="newTransactionBlock">
+                    <div className="newTransaction">
+                        <form>
+                            <input
+                            placeholder="Transaction Name"
+                            type="text"
+                            name="transactionName"
+                            />
+
+                            <div className="inputGroup">
+                                <select name="type">
+                                    <option value="0">Type</option>
+                                    <option value="expense">Expense</option>
+                                    <option value="deposit">Deposit</option>
+                                </select>
+
+                                <input
+                                placeholder="Price"
+                                type="text"
+                                name="Price"
+                            />
+                            </div>
+
+                            <button className="addTransaction"> + Add Transaction</button>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="latestTransactions">
+                    <p>Latest Transactions</p>
+                    <ul>
+                        <li>
+                        <div>ATM Deposit</div>
+                        <div>+$5</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             </>
         )
     }
